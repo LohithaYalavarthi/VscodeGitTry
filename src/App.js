139f002698130ff33f4d99b333367ecdd0bf5321
Lohitha <lohitha@Admins-MacBook-Pro.local>
2020-05-04 19:03:21 +0530

    Committed 3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App () {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Hello Again 4
+          Try react
         </a>
       </header>
     </div>
